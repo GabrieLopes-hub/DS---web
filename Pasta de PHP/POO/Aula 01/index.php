0cf352e72edf37778469a75696cac7ff05beb208
GabrieLopes-hub <gabrielopes.20195@gmail.com>
2025-02-11 10:31:23 -0300

    explicação

---
+++ Pasta de PHP/POO/Aula 01/index.php
@@ -0,0 +1,21 @@
+<?php
+class veiculo {
+    //Atributos:
+    public $marca;
+    public $cor;
+
+    protected $seguro;
+
+    private $renavam;
+
+    //Metodo:
+    public function ligar(){
+
+    }
+}
+//Instanciando meu objeto
+$Polo = new  veiculo(); //inicio di objeto
+$polo -> marca = "Wolkswage"; // atribuindo valo a um atributo
+$polo -> renavam = "123456" 
+
+?>
---
+++ Pasta de PHP/POO/Aula 01/index2.php
@@ -0,0 +1,10 @@
+<?php
+class pessoa(){
+    public $nome; //atributo
+
+    public function falar(){ //Método
+        
+        return "O meu nome é" .$this->nome;
+
+    }
+}
